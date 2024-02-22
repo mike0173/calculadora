a50967315d9b3b4da0b8ebafdef0cf724d4c857f
Mike <miguelagb070@gmail.com>
2024-02-22 11:09:06 -0500

    operacion-multiplicacion

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function sumar(num1, num2) {
 function restar(num1, num2){
     return num1 - num2;
 }
-function multimplicar(num1, num2){
+function multiplicar(num1, num2){
     return num1 * num2;
 }
 function dividir(num1, num2){
