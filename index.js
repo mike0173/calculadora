4113a7ecba0cd190fe21c06190eb86485293822c
Mike <miguelagb070@gmail.com>
2024-02-22 10:43:13 -0500

    operacion-suma

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 console.log("Calculadora");
-// Función principal del programa
-function calculadora() {
-    var opcion = prompt(
-      "Elija una opción:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Factorial\n6. Exponente\n7. Raíz cuadrada"
-    );
-    var num1 = parseFloat(prompt("Ingrese el primer número:"));
-    var num2, resultado;
-  
-    if (opcion !== "5" && opcion !== "7") {
-      num2 = parseFloat(prompt("Ingrese el segundo número:"));
-    }
-  
-    alert(
-      "la operacion es " +
-        opcion +
-        " el primer valor es " +
-        num1 +
-        " el segundo valor es " +
-        num2
-    );
-  }
-  
-  // Llamar a la función principal
-  calculadora();
-  // Función para sumar dos números
+
 function sumar(num1, num2) {
     return num1 + num2;
   }
-  
-  // Función principal del programa
+function restar(num1, num2){
+    return num1 - num2;
+}
+function multimplicar(num1, num2){
+    return num1 * num2;
+}
+function dividir(num1, num2){
+    return num1 / num2;
+}
+function factorial(num1){
+    return num1
+}
   function calculadora() {
     var opcion = prompt(
       "Elija una opción:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División\n5. Factorial\n6. Exponente\n7. Raíz cuadrada"
@@ -70,6 +56,4 @@ function sumar(num1, num2) {
   
     alert("El resultado es: " + resultado);
   }
-  
-  // Llamar a la función principal
   calculadora();
